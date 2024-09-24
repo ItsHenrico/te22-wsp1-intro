ca0ce1f63dedf7ecf7bdbf89e7fa8c97cc76ebc8
ItsHenrico <henrik.forsberg@elev.ga.ntig.se>
2024-09-24 10:01:57 +0200

    gjorde saker tror jag kanske idk

---
+++ routes/index.js
@@ -0,0 +1,70 @@
+import express from "express"
+
+const router = express.Router()
+
+router.get("/test", (req, res) => {
+    res.send("Hello World")
+});
+
+//Allt vårat gammalt skit
+
+router.get("/", (req, res) => {
+    console.log(req.query)
+    const name = req.query.name
+    console.log(name)
+    res.render("index.njk", {
+        message: `Hello ${name}!`,
+        title: `Hem`,
+        github: `https://www.github.com/itshenrico`,
+    })
+})
+
+router.get("/watch", (req, res) => {
+    const movieID = req.query.v
+    console.log(movieID)
+
+    const movies = {
+        "ID": {
+            title: `Cars`,
+            year: 2006,
+            ID: "pQly7_Cj34U",
+        },
+        "ID2": {
+            title: `Cars 2`,
+            year: 2011,
+            ID: "zonotSm4Mdc",
+        },
+        "ID3": {
+            title: `Cars 3`,
+            year: 2017,
+            ID: "2LeOH9AGJQM",
+        }
+    }
+
+    const movie = movies[movieID]
+    console.log(movie)
+
+    res.render("watch.njk", {
+        title: `watch`,
+        movie: movie,
+    })
+    //res.json(movie)
+})
+
+router.get("/ytub", (req, res) => {
+    const ID = req.query.v
+    console.log(ID)
+    res.render("ytub.njk", {
+        title: "YouTube",
+        ID: ID,
+    })
+})
+
+router.get("/about", (req, res) => {
+    res.render("about.njk", {
+        message: `About`,
+        title: `About`,
+    })
+})
+
+export default router
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express"
 import nunjucks from "nunjucks"
+import morgan from "morgan"
+import indexRouter from "./routes/index.js"
 
 const app = express()
 
@@ -8,65 +10,15 @@ nunjucks.configure("views", {
     express: app,
 })
 
+app.use("/", indexRouter)
 app.use(express.static("public"))
+app.use(morgan("dev"))
 
-app.get("/", (req, res) => {
-    console.log(req.query)
-    const name = req.query.name
-    console.log(name)
-    res.render("index.njk", {
-        message: `Hello ${name}!`,
-        title: `Hem`,
-        github: `https://www.github.com/itshenrico`,
-    })
-})
-
-app.get("/watch", (req, res) => {
-    const movieID = req.query.v
-    console.log(movieID)
-
-    const movies = {
-        "ID": {
-            title: `Cars`,
-            year: 2006,
-            ID: "pQly7_Cj34U",
-        },
-        "ID2": {
-            title: `Cars 2`,
-            year: 2011,
-            ID: "zonotSm4Mdc",
-        },
-        "ID3": {
-            title: `Cars 3`,
-            year: 2017,
-            ID: "2LeOH9AGJQM",
-        }
-    }
-
-    const movie = movies[movieID]
-    console.log(movie)
-
-    res.render("watch.njk", {
-        title: `watch`,
-        movie: movie,
-    })
-    //res.json(movie)
-})
-
-app.get("/ytub", (req, res) => {
-    const ID = req.query.v
-    console.log(ID)
-    res.render("ytub.njk", {
-        title: "YouTube",
-        ID: ID,
-    })
-})
-
-app.get("/about", (req, res) => {
-    res.render("about.njk", {
-        message: `About`,
-        title: `About`,
-    })
+app.use((req, res) => {
+   // res.status(404).send("404 - Not Found")
+   res.status(404).render("404.njk", {
+    title: "404 - Not Found"
+   })
 })
 
 app.listen(3000, () => {
